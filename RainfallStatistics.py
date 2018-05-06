8b598a62587ddfabd359b05af71a8f8b719090b1
UncleSam119 <supersam521@gmail.com>
2018-05-06 18:06:36 -0400

    First commit

---
+++ RainfallStatistics.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    sMonthNames = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+               'November', 'December')
+
+    iAvgRainfall = [] * 12
+
+    #for element in iAvgRainfall:
+
+
+
