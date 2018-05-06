561fe5f37d797260c1b7d82f0707b71aff169cb8
UncleSam119 <supersam521@gmail.com>
2018-05-06 19:49:14 -0400

    input loop done

--- RainfallStatistics.py
+++ RainfallStatistics.py
@@ -1,12 +1,26 @@
 
+def getRainFallInput(sMonth):
+    fRainfall = float(input('Enter the rainfall for ' + sMonth + ': '))
+    return fRainfall
+
+
+
 
 def main():
+
+    #Declaring the tuple and the list needed
+
     sMonthNames = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-               'November', 'December')
+                   'November', 'December')
 
-    iAvgRainfall = [] * 12
+    iAvgRainfall = []
 
-    #for element in iAvgRainfall:
+    for element in sMonthNames:
+        iAvgRainfall.append(getRainFallInput(element))
 
+    #print(iAvgRainfall)
 
+        
+
+main()
 
